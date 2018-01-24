162bed28226860686fe26dde1ee3001652e4c939
Izzy <yzziizzy@gmail.com>
2018-01-23 18:07:44 -0700

    beginning of cargo boat

--- mods/boats/init.lua
+++ mods/boats/init.lua
@@ -526,3 +526,331 @@ minetest.register_craft({
 		{"default:tin_ingot", "default:tin_ingot", "default:tin_ingot"},
 	},
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- ---------------------------------------------------
+-- Steel boat
+
+
+
+
+local boat_steel = {
+	physical = true,
+	-- Warning: Do not change the position of the collisionbox top surface,
+	-- lowering it causes the boat to fall through the world if underwater
+	collisionbox = {-2.35, -0.35, -2.35, 02.35, 0.3, 02.35},
+	visual = "mesh",
+	visual_size = {x=2,y=2,z=2},
+	mesh = "boats_barge.obj",
+	textures = {"default_bronze_block.png"},
+
+	driver = nil,
+	v = 0,
+	last_v = 0,
+	removed = false,
+	
+	box = nil,
+}
+
+local boat_cargo_box = {
+	physical = true,
+	-- Warning: Do not change the position of the collisionbox top surface,
+	-- lowering it causes the boat to fall through the world if underwater
+	collisionbox = {-0.4, -0.4, -0.4, 0.4, 0.4, 0.4},
+	visual = "cube",
+	--visual_size = {x=.4, y=.4, z=.4},
+	textures = {
+		"default_chest_top.png",
+		"default_chest_side.png",
+		"default_chest_side.png",
+		"default_chest_side.png",
+		"default_chest_side.png",
+		"default_chest_front.png",
+		},
+
+	driver = nil,
+	v = 0,
+	last_v = 0,
+	removed = false
+}
+
+
+minetest.register_entity("boats:boat_cargo_box", boat_cargo_box)
+
+
+function boat_steel.on_rightclick(self, clicker)
+	if not clicker or not clicker:is_player() then
+		return
+	end
+	local name = clicker:get_player_name()
+	if self.driver and clicker == self.driver then
+		self.driver = nil
+		clicker:set_detach()
+		player_api.player_attached[name] = false
+		player_api.set_animation(clicker, "stand" , 30)
+		local pos = clicker:getpos()
+		pos = {x = pos.x, y = pos.y + 0.2, z = pos.z}
+		minetest.after(0.1, function()
+			clicker:setpos(pos)
+		end)
+	elseif not self.driver then
+		local attach = clicker:get_attach()
+		if attach and attach:get_luaentity() then
+			local luaentity = attach:get_luaentity()
+			if luaentity.driver then
+				luaentity.driver = nil
+			end
+			clicker:set_detach()
+		end
+		self.driver = clicker
+		clicker:set_attach(self.object, "",
+			{x = 0.5, y = 1, z = -3}, {x = 0, y = 0, z = 0})
+		player_api.player_attached[name] = true
+		minetest.after(0.2, function()
+			player_api.set_animation(clicker, "sit" , 30)
+		end)
+		clicker:set_look_horizontal(self.object:getyaw())
+	end
+end
+
+
+function boat_steel.on_activate(self, staticdata, dtime_s)
+	self.object:set_armor_groups({immortal = 1})
+	if staticdata then
+		self.v = tonumber(staticdata)
+	end
+	self.last_v = self.v
+end
+
+
+function boat_steel.get_staticdata(self)
+	return tostring(self.v)
+end
+
+
+function boat_steel.on_punch(self, puncher)
+	if not puncher or not puncher:is_player() or self.removed then
+		return
+	end
+	if self.driver and puncher == self.driver then
+		self.driver = nil
+		puncher:set_detach()
+		player_api.player_attached[puncher:get_player_name()] = false
+	end
+	if not self.driver then
+		self.removed = true
+		local inv = puncher:get_inventory()
+		if not (creative and creative.is_enabled_for
+				and creative.is_enabled_for(puncher:get_player_name()))
+				or not inv:contains_item("main", "boats:boat_steel") then
+			local leftover = inv:add_item("main", "boats:boat_steel")
+			-- if no room in inventory add a replacement boat to the world
+			if not leftover:is_empty() then
+				minetest.add_item(self.object:getpos(), leftover)
+			end
+		end
+		-- delay remove to ensure player is detached
+		minetest.after(0.1, function()
+			self.object:remove()
+		end)
+	end
+end
+
+
+function boat_steel.on_step(self, dtime)
+	self.v = get_v(self.object:getvelocity()) * get_sign(self.v)
+	local yaw = self.object:getyaw()
+	local bp = self.object:getpos()
+	local velo = self.object:getvelocity()
+	bp.y = bp.y + 4
+	--print("lol: " ..self.lol)
+	if not self.box then
+	--	print("adding box")
+		self.box = minetest.add_entity(bp, "boats:boat_cargo_box")
+	--	self.lol = "mej"
+	else
+	--	print("has box")
+		print(yaw)
+		--self.box:setpos(bp)
+		self.box:setyaw(yaw)
+		self.box:setvelocity(velo)
+	
+	end
+	
+	if self.driver then
+		local ctrl = self.driver:get_player_control()
+		local yaw = self.object:getyaw()
+		if ctrl.up then
+			self.v = self.v + 0.2
+		elseif ctrl.down then
+			self.v = self.v - 0.2
+		end
+		if ctrl.left then
+			if self.v < 0 then
+				self.object:setyaw(yaw - (1 + dtime) * 0.04)
+			else
+				self.object:setyaw(yaw + (1 + dtime) * 0.04)
+			end
+		elseif ctrl.right then
+			if self.v < 0 then
+				self.object:setyaw(yaw + (1 + dtime) * 0.04)
+			else
+				self.object:setyaw(yaw - (1 + dtime) * 0.04)
+			end
+		end
+	end
+
+	if self.v == 0 and velo.x == 0 and velo.y == 0 and velo.z == 0 then
+		self.object:setpos(self.object:getpos())
+		return
+	end
+	local s = get_sign(self.v)
+	self.v = self.v - 0.002 * s
+	if s ~= get_sign(self.v) then
+		self.object:setvelocity({x = 0, y = 0, z = 0})
+		self.v = 0
+		return
+	end
+	if math.abs(self.v) > 15 then
+		self.v = 15 * get_sign(self.v)
+	end
+
+	local p = self.object:getpos()
+	p.y = p.y - 0.5
+	local new_velo
+	local new_acce = {x = 0, y = 0, z = 0}
+	if not is_water(p) then
+		local nodedef = minetest.registered_nodes[minetest.get_node(p).name]
+		if (not nodedef) or nodedef.walkable then
+			self.v = 0
+			new_acce = {x = 0, y = 1, z = 0}
+		else
+			new_acce = {x = 0, y = -9.8, z = 0}
+		end
+		new_velo = get_velocity(self.v, self.object:getyaw(),
+			self.object:getvelocity().y)
+		self.object:setpos(self.object:getpos())
+	else
+		p.y = p.y + 1
+		if is_water(p) then
+			local y = self.object:getvelocity().y
+			if y >= 5 then
+				y = 5
+			elseif y < 0 then
+				new_acce = {x = 0, y = 3, z = 0} -- float parameters. bigger = bouncier
+			else
+				new_acce = {x = 0, y = 1, z = 0}
+			end
+			new_velo = get_velocity(self.v, self.object:getyaw(), y)
+			self.object:setpos(self.object:getpos())
+		else
+			new_acce = {x = 0, y = 0, z = 0}
+			if math.abs(self.object:getvelocity().y) < 1 then
+				local pos = self.object:getpos()
+				pos.y = math.floor(pos.y) + 0.5
+				self.object:setpos(pos)
+				new_velo = get_velocity(self.v, self.object:getyaw(), 0)
+			else
+				new_velo = get_velocity(self.v, self.object:getyaw(),
+					self.object:getvelocity().y)
+				self.object:setpos(self.object:getpos())
+			end
+		end
+	end
+	self.object:setvelocity(new_velo)
+	self.object:setacceleration(new_acce)
+	
+
+end
+
+
+
+minetest.register_entity("boats:boat_steel", boat_steel)
+
+
+minetest.register_craftitem("boats:boat_steel", {
+	description = "Steel Boat",
+	inventory_image = "boats_tin_inventory.png^[colorize:#600:80",
+	wield_image = "boats_tin_wield.png",
+	wield_scale = {x = 2, y = 2, z = 1},
+	liquids_pointable = true,
+	groups = {flammable = 2},
+	stack_max = 1,
+
+	on_place = function(itemstack, placer, pointed_thing)
+		local under = pointed_thing.under
+		local node = minetest.get_node(under)
+		local udef = minetest.registered_nodes[node.name]
+		if udef and udef.on_rightclick and
+				not (placer and placer:is_player() and
+				placer:get_player_control().sneak) then
+			return udef.on_rightclick(under, node, placer, itemstack,
+				pointed_thing) or itemstack
+		end
+
+		if pointed_thing.type ~= "node" then
+			return itemstack
+		end
+		if not is_water(pointed_thing.under) then
+			return itemstack
+		end
+		pointed_thing.under.y = pointed_thing.under.y + 0.5
+		boat = minetest.add_entity(pointed_thing.under, "boats:boat_steel")
+		if boat then
+			if placer then
+				boat:setyaw(placer:get_look_horizontal())
+			end
+			local player_name = placer and placer:get_player_name() or ""
+			if not (creative and creative.is_enabled_for and
+					creative.is_enabled_for(player_name)) then
+				itemstack:take_item()
+			end
+			
+			--local p = {x=pointed_thing.under.x, y=pointed_thing.under.y, z=pointed_thing.under.z}
+			
+		--	//boat.box = minetest.add_entity(p, "boats:boat_cargo_box")
+-- 			if box then
+-- 				box:set_attach(boat, "", {x=5,y=8,z=0}, {x=0,y=0,z=0})
+-- 			else
+-- 				print("failed to attach cargo box")
+-- 			end
+		end
+		return itemstack
+	end,
+})
+
+
+
+
+minetest.register_craft({
+	output = "boats:boat_steel",
+	recipe = {
+		{"",           "",           ""          },
+		{"default:steel_ingot", "",           "default:steel_ingot"},
+		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+	},
+})
